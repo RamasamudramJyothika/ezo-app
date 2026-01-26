1d1c289ffd0cdd55c9fda2f1145afa891a235ba6
RamasamudramJyothika <ramasamudramjyothika462003@gmail.com>
2026-01-26 20:35:39 +0530

    added new product

--- src/Data/products.js
+++ src/Data/products.js
@@ -1,12 +1,12 @@
 import BlackTea from "../assets/Black Tea.png";
 import GreenTea from "../assets/Green Tea.png";
-import Espresso from "../assets/Espresso.png";
+import Espressocoffee from "../assets/Espressocoffee.png";
 import Cookies from "../assets/Cookies.png";
 
 const products = [
   { id: 1, name: "Black Tea", categoryId: 1, price: 20, image: BlackTea },
   { id: 2, name: "Green Tea", categoryId: 1, price: 25, image: GreenTea },
-  { id: 3, name: "Espresso", categoryId: 2, price: 50, image: Espresso },
+  { id: 3, name: "Espresso coffee", categoryId: 2, price: 50, image: Espressocoffee },
   { id: 4, name: "Cookies", categoryId: 3, price: 15, image: Cookies }
 ];
 export default products;
