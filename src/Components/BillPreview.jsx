3c734679ed0f4f922530577ead6120f9ab548d16
RamasamudramJyothika <ramasamudramjyothika462003@gmail.com>
2026-01-27 19:45:03 +0530

    modified

--- src/Components/BillPreview.jsx
+++ src/Components/BillPreview.jsx
@@ -6,6 +6,7 @@ import DialogTitle from '@mui/material/DialogTitle';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+
 function BillPreview({open, onClose, cart, clearCart}){
     const totalAmount = cart.reduce(
         (sum, item) => sum + item.price * item.quantity, 0
